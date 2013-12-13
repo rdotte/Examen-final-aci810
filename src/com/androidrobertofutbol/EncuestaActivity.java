3753e7d1fa98b78bd05744304300c8c1065e8caa
rdotte <r.valdesdotte@gmail.com>
2013-12-13 15:36:19 -0400

    Examen2

--- src/com/androidrobertofutbol/EncuestaActivity.java
+++ src/com/androidrobertofutbol/EncuestaActivity.java
@@ -2,7 +2,10 @@ package com.androidrobertofutbol;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class EncuestaActivity extends Activity {
 
@@ -15,8 +18,13 @@ public class EncuestaActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.encuesta, menu);
+		getMenuInflater().inflate(R.menu.favoritos, menu);
 		return true;
 	}
 
+	public void mandardatos(View view) {
+        Intent i = new Intent(this, AplicacionFutbol.class);
+        Toast.makeText(this, "Formulario Enviado", Toast.LENGTH_LONG).show();
+        this.startActivity(i);
+}
 }
